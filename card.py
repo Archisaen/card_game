b5b6e3b53039841318b5268946bff965cdfc2b30
Archibald Saen <archibaldsaen98@gmail.com>
2023-07-12 00:05:44 +0200

    pushing all files with instructions

---
+++ card.py
@@ -0,0 +1,2 @@
+Card Module: Create a module (e.g., card.py) to define the Card class representing individual playing cards.
+Each card object can have attributes like rank (e.g., Ace, 2, 3, ...) and suit (e.g., Hearts, Diamonds, ...).
---
+++ player.py
@@ -0,0 +1,2 @@
+Player Module: Create a module (e.g., player.py) to define the Player class. Each player object can have attributes like name, hand (cards held by the player), and possibly a score.
+The Player class should handle operations specific to individual players, such as playing a card, drawing a card, or managing their hand.
